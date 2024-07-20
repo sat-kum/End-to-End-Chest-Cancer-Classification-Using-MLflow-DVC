a730cf4fb9abbaf44e37e95755f34299d929abac
sat-kum <74048819+sat-kum@users.noreply.github.com>
2024-07-21 00:01:55 +0530

    folder structure

---
+++ template.py
@@ -0,0 +1,44 @@
+import os 
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level= logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "CNNClassifier"
+
+list_of_files= [
+    ".github/workflow/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for file in list_of_files:
+    file = Path(file)
+    filedir, filename = os.path.split(file)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(file)) or (os.path.getsize(file)==0):
+        with open(file, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {file}")
+
+    else:
+        logging.info(f"{filename} is already exists")
